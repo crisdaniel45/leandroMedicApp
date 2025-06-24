c905ac040c3773b14df0e5a11a451b95e2c18265
daniel <leandroaybarccristian@.com>
2025-06-23 20:42:31 -0500

    arreglando nombres de las tablas

--- src/main/java/com/clinica/citasmedicas/model/Medico.java
+++ src/main/java/com/clinica/citasmedicas/model/Medico.java
@@ -6,14 +6,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "Medico")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class Medico {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
